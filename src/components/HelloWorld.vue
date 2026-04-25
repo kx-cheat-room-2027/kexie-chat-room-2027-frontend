d18394ee67ad2ba49d2d749478b82bc2def0bd62
Leaves_awa <bzbz233nb@163.com>
2026-04-25 21:25:27 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ const count = ref(0)
 
 <template>
   <section id="center">
-    <div class="her">
+    <div class="hero">
       <img :src="heroImg" class="base" width="170" height="179" alt="" />
       <img :src="vueLogo" class="framework" alt="Vue logo" />
       <img :src="viteLogo" class="vite" alt="Vite logo" />
